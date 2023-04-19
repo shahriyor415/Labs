541981218636e657353b87dd73b85e1f854c7204
Shahriyor415 <90951763+shahriyor415@users.noreply.github.com>
2023-04-19 19:11:24 +0500

    Add files via upload

---
+++ Rabota_4/RTOS/RtosWrapper/main.cpp
@@ -0,0 +1,82 @@
+#include "rtos.hpp"         // for Rtos
+#include "mailbox.hpp"      // for Mailbox
+#include "event.hpp"        // for Event
+
+//#include "mytask.hpp"       // for MyTask
+//#include "led1task.hpp"     // for Led1Task
+//#include "myfirsttask.h"    // for MyFirstTask
+//#include "buttontask.h"   //for ButtonTask
+#include "rccregisters.hpp" // for RCC
+
+//#include "Application/Diagnostic/GlobalStatus.hpp"
+//#include "gpioaregisters.hpp"  // for GPIOA
+#include "gpiocregisters.hpp"  // for GPIOC
+
+#include "myled.h"
+#include "MyLED9.h"
+
+std::uint32_t SystemCoreClock = 16'000'000U;
+
+
+extern "C" {
+int __low_level_init(void)
+{
+  //Switch on external 16 MHz oscillator
+  RCC::CR::HSION::On::Set();
+ // while (RCC::CR::HSIRDY::NotReady::IsSet())
+  {
+
+  }
+  //Switch system clock on external oscillator
+  RCC::CFGR::SW::Hsi::Set();
+//  while (!RCC::CFGR::SWS::Hsi::IsSet())
+ {
+
+  }
+  //Switch on clock on PortA and PortC
+  RCC::AHB1ENRPack<
+      RCC::AHB1ENR::GPIOCEN::Enable
+     // RCC::AHB1ENR::GPIOAEN::Enable
+  >::Set();
+
+  RCC::APB2ENR::SYSCFGEN::Enable::Set();
+
+  //LED1 on PortA.5, set PortA.5 as output
+  //GPIOA::MODER::MODER5::Output::Set();
+
+  /* LED2 on PortC.9, LED3 on PortC.8, LED4 on PortC.5 so set PortC.5,8,9 as output */
+  GPIOC::MODERPack<
+     // GPIOC::MODER::MODER5::Output,
+      GPIOC::MODER::MODER8::Output,
+      GPIOC::MODER::MODER9::Output
+  >::Set();
+
+  return 1;
+} 
+}
+
+//OsWrapper::Event event{500ms, 1};
+
+//MyTask myTask(event, UserButton::GetInstance());
+//Led1Task led1Task(event, LedsController::GetInstance());
+
+//OsWrapper::Event buttonEvent(500ms, 1);
+//OsWrapper::MailBox<int, 1> buttonMailBox;
+//ButtonTask buttonTask(buttonMailBox);
+//MyFirstTask myFirstTask;
+MyLED<GPIOC::ODR, 8> myledTask(200ms); 
+MyLED<GPIOC::ODR, 9> myled9Task(100ms); 
+
+int main()
+{
+  using namespace OsWrapper;
+  //Rtos::CreateThread(myTask, "myTask", ThreadPriority::lowest);
+ // Rtos::CreateThread(led1Task, "Led1Task");
+//  Rtos::CreateThread(myFirstTask, "MyFirstTask", ThreadPriority::highest);
+//  Rtos::CreateThread(buttonTask, "ButtonTask", ThreadPriority::normal);
+    Rtos::CreateThread(myledTask, "myledTask");
+    Rtos::CreateThread(myled9Task, "myled9Task",ThreadPriority::highest);
+   Rtos::Start();
+
+  return 0;
+}
